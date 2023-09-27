cd961e2abbf090578106da7c02e0616d0b897ecd
Andrey Miskov <amiskov@gmail.com>
2023-09-27 06:04:48 +0300

    improve admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -12,9 +12,9 @@ class RecipeIngredientInline(admin.StackedInline):
     model = RecipeIngredients
     extra = 3
     verbose_name = 'Ингредиент'
-    verbose_name_plural = 'Ингредиенты'
+    verbose_name_plural = 'Состав рецепта'
     model.ingredient.field.verbose_name = 'Ингредиент'
-    model.__str__ = lambda _: 'Идентификатор ингредиента'
+    model.__str__ = lambda _: ''
 
 
 @admin.register(Recipe)
