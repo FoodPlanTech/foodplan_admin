ab8ede3b5d61cf756bc3c65a58f4b63cf61f83f3
Andrey Miskov <amiskov@gmail.com>
2023-09-28 23:23:04 +0300

    add telegram user admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser
 from recipes.models import Recipe
+
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import CustomUser, TelegramUser
+
+
+@admin.register(TelegramUser)
+class TelegramUserAdmin(admin.ModelAdmin):
+    list_display = ('telegram_id', 'created_at', 'user')
 
 
 class LikedRecipeInline(admin.TabularInline):
@@ -26,6 +32,7 @@ class DislikedRecipeInline(admin.TabularInline):
     model.__str__ = lambda _: 'нелюбимый рецепт'
 
 
+@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
@@ -33,12 +40,10 @@ class CustomUserAdmin(UserAdmin):
     list_display = [
         "username",
         "email",
+        "telegram",
         "is_staff",
     ]
     inlines = [LikedRecipeInline, DislikedRecipeInline]
     raw_id_fields = ('liked_recipes', 'disliked_recipes',)
     fieldsets = UserAdmin.fieldsets
     add_fieldsets = UserAdmin.add_fieldsets
-
-
-admin.site.register(CustomUser, CustomUserAdmin)
