5fd4f64ba08dcfc28bbb0ca00968acc586842a53
Andrey Miskov <amiskov@gmail.com>
2023-09-30 18:43:11 +0300

    improve errors for current recipe

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -2,6 +2,7 @@ import random
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
+from rest_framework.exceptions import ValidationError
 from .models import Recipe, FoodPlan, Preference
 from accounts.models import TelegramAccount
 from subscriptions.models import Subscription
@@ -48,8 +49,16 @@ class GetRecipeQuerySerializer(serializers.Serializer):
         telegram_id = validated_data.get('telegram_id')
 
         foodplan = FoodPlan.objects.filter(pk=telegram_id) \
-            .prefetch_related('preferences')[0]
-        preferences = foodplan.preferences.all()
+            .prefetch_related('preferences')
+        if not foodplan:
+            raise ValidationError(
+                f'FoodPlan для пользователя {telegram_id} не найден.')
+
+        preferences = foodplan[0].preferences.all()
+        if not preferences:
+            raise ValidationError(
+                f'Предпочтения пользователя {telegram_id} не заполнены.')
+
         recipes = Recipe.objects.filter(preferences__in=preferences)
         recipe = random.choice(recipes)
         return RecipeSerializer(recipe, context={'request': request}).data
